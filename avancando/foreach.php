fa43968553cb5ee0706b67df10943b17bb5261dd
Lucas Vilharba <127902781+lvilharba@users.noreply.github.com>
2023-08-25 13:55:50 -0300

    Adicionando valores a um array associativo.

--- avancando/foreach.php
+++ avancando/foreach.php
@@ -1,18 +1,23 @@
 <?php
 
 $contasCorrentes = [
-    123456789 => [
+    '123456789-10' => [
         'titular' => 'Lucas',
         'saldo' => 1000
     ],
-    123456788 => [
+    '123456788=11' => [
         'titular' => 'Maria',
         'saldo' => 10000,
     ],
-    123546789 => [
+    '123546789-12' => [
         'titular' => 'Mateus',
         'saldo' => 300
     ]
+];
+
+$contasCorrentes['987654321-00'] = [
+    'titular' => 'Heloisa',
+    'saldo' => 3500
 ];
 
 foreach ($contasCorrentes as $cpf => $conta) {
