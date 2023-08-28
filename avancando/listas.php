f47a15613d6b45b92f786bc6d1dc3a5beb5bc9a2
Lucas Vilharba <127902781+lvilharba@users.noreply.github.com>
2023-08-28 15:11:31 -0300

    Testando a função list.

--- avancando/listas.php
+++ avancando/listas.php
@@ -1,12 +1,9 @@
 <?php
 
-$idade1 = 21;
-$idade2 = 23;
-$idade3 = 19;
-$idade4 = 21;
-$idade5 = 30;
-$idade6 = 25;
-
 $idadeList = [21, 23, 19, 21, 30, 25];
 
-$primeiraIdade = $idadeList[0];
+list($idadeLucas, $idadeJulia, $idadeJoao) = $idadeList;
+
+echo $idadeLucas . PHP_EOL;
+echo $idadeJulia . PHP_EOL;
+echo $idadeJoao . PHP_EOL;
