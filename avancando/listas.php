cea362748923352eec18628517e0dc10e3aef63d
Lucas Vilharba <127902781+lvilharba@users.noreply.github.com>
2023-08-28 15:38:57 -0300

    Testando a função unset.

--- avancando/listas.php
+++ avancando/listas.php
@@ -2,8 +2,14 @@
 
 $idadeList = [21, 23, 19, 21, 30, 25];
 
-list($idadeLucas, $idadeJulia, $idadeJoao) = $idadeList;
+/*list($idadeLucas, $idadeJulia, $idadeJoao) = $idadeList;
 
 echo $idadeLucas . PHP_EOL;
 echo $idadeJulia . PHP_EOL;
-echo $idadeJoao . PHP_EOL;
+echo $idadeJoao . PHP_EOL;*/
+
+unset($idadeList[3]);
+
+foreach ($idadeList as $idade) {
+    echo $idade . PHP_EOL;
+}
