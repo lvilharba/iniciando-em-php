67b6891ba70dffc691105ed8230de83d79305996
Lucas Vilharba <127902781+lvilharba@users.noreply.github.com>
2023-10-16 17:49:08 -0300

    Relacionando a classe Titular com a classe CPF.

--- introducaoPOO/Titular.php
+++ introducaoPOO/Titular.php
@@ -3,21 +3,31 @@
 class titular
 {
     private $cpf;
-    private $nome;
+    private string $nome;
 
-    public function __construct(string $cpf, string $nome)
+    public function __construct(Cpf $cpf, string $nome)
     {
-        $this->cpf = $cpf; 
+        $this->cpf = $cpf;
+        $this->validaNomeTitular($nome);
         $this->nome = $nome; 
     }
 
-    public function getCpf(): string
-    {
-        return $this->cpf;
-    }
+
 
     public function getNome(): string
     {
         return $this->nome;
+    }
+    private function validaNomeTitular(string $nomeTitular)
+    {
+        if (strlen($nomeTitular) < 5) {
+            echo "Nome precisa ter pelo menos 5 caracteres.";
+            exit();
+        }
+    }
+
+    public function getCpf(): string
+    {
+        return $this->cpf->getCpf();
     }
 }
